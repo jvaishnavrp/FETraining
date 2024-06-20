01548764acdce97ae6c47b09a2f8a22dc67b5a73
Lalit Gour <lgour@rightpoint.com>
2024-06-20 22:59:22 +0530

    Added gulp folder structure

---
+++ Assets/js/main.js
@@ -0,0 +1,11 @@
+import App from './app.js';
+import bannerCarouselComponent from './banner-carousel.js';
+import bannerVideoComponent from './banner-video.js';
+import newFn from './new.js';
+
+const app = new App();
+
+const bannerCarouselSlider = bannerCarouselComponent();
+
+const plasticBannerVideoComp = bannerVideoComponent();
+const newFnComp = newFn();
