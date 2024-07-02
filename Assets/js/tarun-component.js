e14a74c3630e9d0af6948145a73de798f77fee23
Tarun Kumawat <tarunkumawat@rightoint.com>
2024-07-02 21:17:11 +0530

    updated carousel

--- Assets/js/tarun-component.js
+++ Assets/js/tarun-component.js
@@ -12,4 +12,33 @@ document.addEventListener('DOMContentLoaded', function() {
       overlay.classList.remove('active');
     });
   });
-  
+
+
+  //Carousel
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  const slides = document.querySelector('.carousel__slides');
+  const slideItems = document.querySelectorAll('.carousel__slides__slide');
+  const totalSlides = slideItems.length;
+  let currentIndex = 0;
+  let slideWidth = slideItems[0].getBoundingClientRect().width; // Get the width of the first slide
+
+  function goToSlide(index) {
+    currentIndex = index;
+    slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+  }
+
+  prevButton.addEventListener('click', () => {
+   currentIndex > 0 ? currentIndex-- : (currentIndex = totalSlides - 1);
+   goToSlide(currentIndex);
+  });
+
+  nextButton.addEventListener('click', () => {
+    (currentIndex < totalSlides - 1) ? currentIndex++ : (currentIndex = 0);
+    goToSlide(currentIndex)
+  });
+
+  window.addEventListener('resize', () => {
+    slideWidth = slideItems[0].getBoundingClientRect().width;
+    goToSlide(currentIndex);
+  });
