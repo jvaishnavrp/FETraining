d79e73ee2b1024505f9056878d10bc23618214af
ranjanayadav56 <ranjanayadav@rightpoint.com>
2024-07-06 22:14:14 +0530

    Resolved some styling issues and responsive for Header component

---
+++ Assets/js/header.js
@@ -0,0 +1,26 @@
+const headerFn = () => {
+    const burgerBtns = document.querySelectorAll(".burger");
+    const closeIcons = document.querySelectorAll(".close-icon");
+    const menuItems = document.querySelector(".header_wrapper_menu");
+
+    const openMenu = () => {
+        burgerBtns.forEach(burgerBtn => {
+            burgerBtn.onclick = () => {
+                menuItems.classList.add("is-active");
+            };
+        });
+    };
+
+    const closeMenu = () => {
+        closeIcons.forEach(closeIcon => {
+            closeIcon.onclick = () => {
+                menuItems.classList.remove("is-active");
+            };
+        });
+    };
+
+    openMenu();
+    closeMenu();
+};
+
+export default headerFn;
